8c28fea65b5ed98a509ef0a56e0ffccd48e84d79
Lev Alhazov <alhazovlev@gmail.com>
2020-10-22 17:06:38 +0300

    Added person header

---
+++ Person.h
@@ -0,0 +1,16 @@
+#pragma once
+#pragma once
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <typeinfo>
+
+using namespace std;
+
+class Person
+{
+protected:
+	string name;
+	long id;
+	int age;
+};
